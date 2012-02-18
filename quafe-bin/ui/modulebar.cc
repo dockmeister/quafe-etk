755a65b5d912259ea11126339f2f738a1ec945de
Christoph Nikic <cn@gmx.de>
2012-02-18 14:18:41 +0100

    * adding license texts: gpl-v3 * autotools build support

--- quafe-bin/ui/modulebar.cc
+++ quafe-bin/ui/modulebar.cc
@@ -1,9 +1,22 @@
-/* 
- * File:   ModuleBar.cc
- * Author: cn
+/**
  * 
- * Created on 14. August 2010, 14:29
+ *  Quafe - Eve tools for linux.
+ *  Copyright (C) 2012 Christoph Nikic
+ *
+ *  This program is free software: you can redistribute it and/or modify
+ *  it under the terms of the GNU General Public License as published by
+ *  the Free Software Foundation, either version 3 of the License, or
+ *  (at your option) any later version.
+ *
+ *  This program is distributed in the hope that it will be useful,
+ *  but WITHOUT ANY WARRANTY; without even the implied warranty of
+ *  MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ *  GNU General Public License for more details.
+ *
+ *  You should have received a copy of the GNU General Public License
+ *  along with this program.  If not, see <http://www.gnu.org/licenses/>.
  */
+
 
 #include "modulebar.h"
 #include "log.h"
